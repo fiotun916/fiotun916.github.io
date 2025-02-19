d6f4a9f94706468df192bee5e4442821e1a340a3
Fionina Tung <111818287+fiotun916@users.noreply.github.com>
2025-02-19 12:54:07 -0800

    init commit

---
+++ src/components/resume/education.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Educations()
+{
+    return (
+        <div className="p-1 mb-16">
+            <h5 className="mb-2 text-m font-bold tracking-tight"></h5>
+            <hr className="h-px my-8 bg-black border-0 seperator mt-0 mb-0"/>
+            <div className="mb-3 font-normal">
+                <strong>California State University, Dominguez Hills</strong>
+                <p className="text-sm font-light">Bachelors of Science, 2028</p>
+                <p className="text-sm font-light italic">January 2025 - May 2028</p>
+                <p className="text-sm font-light font">
+                    At CSUDH, I'm pursuing a Bachelors of Science in Computer Science.
+                </p>
+            </div>
+        </div>
+    )
+}
