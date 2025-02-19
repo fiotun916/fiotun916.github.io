a1ec9bf1fe575c07d4b8ef76f1842be49ad244d2
Fionina Tung <111818287+fiotun916@users.noreply.github.com>
2025-02-19 12:53:22 -0800

    init commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Educations from "@/components/resume/education";
+
+export default function Home()
+{
+  return(
+    <div className="h-screen p-4 lg:p-8 mt-16">
+      <Educations />
+    </div>
+  )
+}
