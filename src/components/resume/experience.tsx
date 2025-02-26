83c2122b560ea691e7d97bac75251ba8142183e4
Fionina Tung <fioninatung@MacBook-Pro-3.local>
2025-02-26 12:24:47 -0800

    add experience component

---
+++ src/components/resume/experience.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Experience()
+{
+    return (
+        <div className="p-1 mb-16">
+            <h5 className="mb-2 text-m font-bold tracking-tight"></h5>
+            <hr className="h-px my-8 bg-black border-0 seperator mt-0 mb-0"/>
+            <div className="mb-3 font-normal">
+                <strong>DigiTrust</strong>
+                <p className="text-sm font-light">PR Intern</p>
+                <p className="text-sm font-light italic">July 2024 - December 2024</p>
+                <p className="text-sm font-light font">
+                    DigiTrust is a cybersecurity company based in LA. The main purpose and skills of an intern there is to train and groom the skills needed to do ethical hacking. The PR department pitches the concepts for companies in need of penetration tests.
+                </p>
+            </div>
+        </div>
+    )
+}
